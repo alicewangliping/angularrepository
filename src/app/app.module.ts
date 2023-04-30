415c76d1577992d0e9c6968bd3333b028852ef56
alice wang <63572159+alicewangliping@users.noreply.github.com>
2023-04-30 10:15:56 -0400

    commit3: applpic ,footer+logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './footer/footer/footer.component';
+
 @NgModule({
   declarations: [
-
+  AppComponent,
+  BannerComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/banner/banner.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css']
+})
+export class BannerComponent {
+  banner = {
+    heading: "Spring season is back!",
+    subHeading: "And so is our offers too. So why waiting?",
+    cta: "Check out all Products",
+    url: "https://www.amazon.ca/"
+  }
+
+  handleClick() {
+    console.log("clicked the button")
+  }
+}
